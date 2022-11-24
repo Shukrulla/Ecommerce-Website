102307e8900ce173ee9d15c95c6cb88f39c1f47e
Asadov <asadov@Asadovs-MacBook-Pro.local>
2022-11-24 23:56:48 +0100

    way3

---
+++ app.js
@@ -0,0 +1,40 @@
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
+// open cart
+cartIcon.onclick = () => {
+    cart.classList.add("active");
+};
+// close cart
+closeCart.onclick = () => {
+    cart.classList.remove("active");
+};
+
+// cart working js
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded',ready);
+}else {
+    ready();
+}
+
+// Making Function
+function ready() {
+    // Reomve Items from cartIcon
+    var reomveCartButtons = document.getElementsByClassName('cart-remove')
+    console.log(reomveCartButtons)
+    for (var i = 0; i < reomveCartButtons.length; i++) {
+        var button = reomveCartButtons[i];
+        button.addEventListener("click", removeCartItem);
+    }
+}
+
+// reomve items from cart
+function removeCartItem(event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+}
+
+// update total
+function updatetotal() {
+
+}
